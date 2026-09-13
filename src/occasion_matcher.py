bc321029360e62b0454581550e45b65c295c4ee9
Nitha003 <nithaantov@gmail.com>
2026-09-13 11:27:24 +0530

    Add Flask recommendation API

--- src/occasion_matcher.py
+++ src/occasion_matcher.py
@@ -1,6 +1,6 @@
 def get_occasion_score(clothing, selected_occasion):
 
-    if selected_occasion in clothing["occasion"]:
+    if clothing["occasion"].lower() == selected_occasion.lower():
         return 100
 
     return 40
@@ -9,18 +9,24 @@ def get_occasion_score(clothing, selected_occasion):
 def get_weather_score(clothing, weather):
 
     if weather == "Hot":
+
         if clothing["season"] == "Summer":
             return 100
+
         elif clothing["season"] == "Winter":
             return 30
+
         else:
             return 70
 
     if weather == "Cold":
+
         if clothing["season"] == "Winter":
             return 100
+
         elif clothing["season"] == "Summer":
             return 30
+
         else:
             return 70
 
@@ -29,13 +35,15 @@ def get_weather_score(clothing, weather):
 
 def get_activity_score(clothing, activity):
 
-    if activity in clothing["activity"]:
+    if clothing["activity"].lower() == activity.lower():
         return 100
 
     return 50
+
+
 def get_style_score(clothing, selected_style):
 
-    if clothing["style"] == selected_style:
+    if clothing["style"].lower() == selected_style.lower():
         return 100
 
     return 50
